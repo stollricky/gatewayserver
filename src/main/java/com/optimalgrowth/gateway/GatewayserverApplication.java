0ff735aeda0326fae796f3e7d02481988dffcdc2
Ricky Stoll <rwstoll@iu.edu>
2023-04-24 17:59:38 -0400

    lab

---
+++ src/main/java/com/optimalgrowth/gateway/GatewayserverApplication.java
@@ -0,0 +1,15 @@
+package com.optimalgrowth.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+}
